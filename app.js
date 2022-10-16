7965e4ba99fa40cdb2c8431ac3232010dfa488c0
hamdi yahyaoui <hamdi_yahyaoui@yahoo.fr>
2022-10-16 12:46:49 +0100

    copmlete user story 3 and  enhancements

--- app.js
+++ app.js
@@ -1,72 +1,84 @@
-const data = require('./data');
+let data = require('./data');
 
 'use strict'
 
 const args = process.argv
 
 function isEmpty(arr) {
-    return (Array.isArray(arr) && arr.length)
+  return (Array.isArray(arr) && arr.length)
 }
 
 // This function filters out every animal that does not match the string pattern
 const removeNonMatching = (searchedStr, person) => {
-    return person.animals.map((animal) => {
-        if (animal.name.includes(searchedStr)) {
-            return animal;
-        }
+  return person.animals.map((animal) => {
+      if (animal.name.includes(searchedStr)) {
+        return animal;
+      }
     }).filter(e => e)
 }
 
 const filter = (searchedStr) => {
-    const newList = data.filter(q => {
-        let newCountry = q
-        newCountry.people = q.people.filter(p => {
-            let newPerson = p
-            newPerson.animals = removeNonMatching(searchedStr, p)
+  const newList = data.filter(q => {
+    let newCountry = q
+    newCountry.people = q.people.filter(p => {
+      let newPerson = p
+      newPerson.animals = removeNonMatching(searchedStr, p)
 
-            // The 'animals' entry will be removed if there is nothing left inside
-            return isEmpty(newPerson.animals)
-        })
+      // The 'animals' entry will be removed if there is nothing left inside
+      return isEmpty(newPerson.animals)
+    })
 
-        // The 'people' entry will be removed if there is nothing left inside
-        return (isEmpty(newCountry.people))
-    });
+    // The 'people' entry will be removed if there is nothing left inside
+    return isEmpty(newCountry.people)
+  });
 
-    // prints out the filtered list if there is any match
-    console.log((!isEmpty(newList)) ? 'Nothing found' : JSON.stringify(newList))
-    return (!isEmpty(newList)) ? 'Nothing found' : JSON.stringify(newList)
+  // prints out the filtered list if there is any match
+  return !isEmpty(newList) ? 'Nothing found' : JSON.stringify(newList)
+}
+// This function finds a strings in an array
+const find = (array, string) => {
+  return array.find((element) => element.includes(string))
 }
 
+const findFilter = find(args, 'filter') ? find(args, 'filter') : null
+const findCount = find(args, 'count') ? find(args, 'count') : null
+const filterStr = findFilter ? findFilter.split('=') : null
+
+//extract the string we want to filter with
+const containingStr = filterStr ? filterStr[1] : null
+
+//if we have a filter we filter the data with otherwise we return the initial data
+ data =
+ findCount && findFilter ? JSON.parse(filter(containingStr)) : data
+
 const count = () => {
-    const newList = data.map((country) => {
-        country.people.map((person) => {
-            person.name = `${person.name} [${person.animals.length}]`
-            return person
-        })
-        country.name = `${country.name} [${country.people.length}]`
-        return country
+  const newList = data.map((country) => {
+    country.people.map((person) => {
+      person.name = `${person.name} [${person.animals.length}]`
+      return person
     })
-    console.log(JSON.stringify(newList))
-    return JSON.stringify(newList)
+    country.name = `${country.name} [${country.people.length}]`
+    return country
+  })
+  return JSON.stringify(newList)
 }
 
 // USAGE: node app.js --filter=[PATTERN] OR node app.js filter=[PATTERN]
 // USAGE: node app.js --count OR node app.js count
-
+// USAGE: node app.js --filter=[PATTERN]  --count OR --count --filter=[PATTERN]
 try {
-    const cmd = args[2].split("=");
-    if (cmd[0] === '--filter' || cmd[0] === 'filter') {
-        filter(cmd[1])
-    } else if (cmd[0] === '--count' || cmd[0] === 'count') {
-        count()
-    } else {
-        console.log('Wrong arguments')
-    }
-} catch(err) {
-    throw err
+  if (findCount) {
+    console.log(count())
+  } else if (!findCount && findFilter) {
+    console.log(filter(containingStr))
+  } else {
+    console.log('Wrong arguments')
+  }
+} catch (err) {
+  throw err
 }
 
-
 module.exports = {
-    count, filter
+  count,
+  filter,
 }
